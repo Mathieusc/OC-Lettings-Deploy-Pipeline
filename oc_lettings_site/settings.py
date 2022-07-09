39d32daa9925bfda28ddbf4fd6e66f602cb7654e
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-07-09 02:41:32 +0200

    flake8 fix

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,7 +12,7 @@ if os.environ.get('ENV') != 'DEPLOYMENT':
     SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = False
 else:
-    SECRET_KEY='fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+    SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
     DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
