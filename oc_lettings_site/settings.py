5004db5a9339af119b935378ce808ccbfd27ba95
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-07-21 00:27:21 +0200

    setting up environment variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #     DEBUG = False
 # else:
 
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
