8f994b1feaaeafb3d2ffc59d0fbbb0479a0eea6d
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-07-21 03:57:27 +0200

    setting up environment variables

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -9,6 +9,6 @@ from django.shortcuts import render
 # Phasellus eleifend ex auctor venenatis tempus.
 # Aliquam vitae erat ac orci placerat luctus.
 # Nullam elementum urna nisi, pellentesque iaculis enim cursus in.
-# Praesent volutpat porttitor magna, non finibus neque cursus id..
+# Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
