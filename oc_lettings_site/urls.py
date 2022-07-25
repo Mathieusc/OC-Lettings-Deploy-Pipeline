c4ab67d691f30658173e3fcb994c57e3a38f7ad8
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-07-25 18:35:13 +0200

    added sentry trigger error

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 
 from . import views
 
+
 def trigger_error(request):
     """Error verification for Sentry."""
     division_by_zero = 1 / 0
+    return division_by_zero
+
 
 urlpatterns = [
     path('', views.index, name='index'),
